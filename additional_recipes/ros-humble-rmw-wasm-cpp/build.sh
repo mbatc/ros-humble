1faf1a1194efebbfea819dafce1fe793e7654ec2
Tobias Fischer <info@tobiasfischer.info>
2024-01-02 13:34:58 +1000

    Various fixes

--- additional_recipes/ros-humble-rmw-wasm-cpp/build.sh
+++ additional_recipes/ros-humble-rmw-wasm-cpp/build.sh
@@ -1,17 +1,16 @@
+#!/bin/bash
+
 export CONDA_BUILD_CROSS_COMPILATION="1"
 
-echo "set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)">> $SRC_DIR/__vinca_shared_lib_patch.cmake
-echo "set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS \"-s SIDE_MODULE=1 -sWASM_BIGINT\")">> $SRC_DIR/__vinca_shared_lib_patch.cmake
-echo "set(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS \"-s SIDE_MODULE=1 -sWASM_BIGINT\")">> $SRC_DIR/__vinca_shared_lib_patch.cmake
+echo "set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)"> $SRC_DIR/__vinca_shared_lib_patch.cmake
+echo "set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS \"-s SIDE_MODULE=1 -sWASM_BIGINT -s USE_PTHREADS=0 -s DEMANGLE_SUPPORT=1 -s ALLOW_MEMORY_GROWTH=1 \")">> $SRC_DIR/__vinca_shared_lib_patch.cmake
+echo "set(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS \"-s SIDE_MODULE=1 -sWASM_BIGINT -s USE_PTHREADS=0 -s DEMANGLE_SUPPORT=1 -s ALLOW_MEMORY_GROWTH=1 \")">> $SRC_DIR/__vinca_shared_lib_patch.cmake
 echo "set(CMAKE_STRIP FALSE)  # used by default in pybind11 on .so modules">> $SRC_DIR/__vinca_shared_lib_patch.cmake
+echo "set(CMAKE_EXE_LINKER_FLAGS \"-sMAIN_MODULE=1 -sASSERTIONS=1 -fexceptions -lembind -sWASM_BIGINT -s USE_PTHREADS=0 -s DEMANGLE_SUPPORT=1 -sALLOW_MEMORY_GROWTH=1 -L$SRC_DIR/build -L$PREFIX/lib\")  # remove SIDE_MODULE from exe linker flags">> $SRC_DIR/__vinca_shared_lib_patch.cmake
 
 export USE_WASM=ON
-export Python_EXECUTABLE="$BUILD_PREFIX/bin/python"
-export Python_INCLUDE_DIR="$PREFIX/include/python3.11"
-export Python_LIBRARY="$PREFIX/lib/python3.11"
 export EXTRA_CMAKE_ARGS=" \
     -DTHREADS_PREFER_PTHREAD_FLAG=TRUE\
-    -DPython_SITELIB=$SP_DIR       \
     -DCMAKE_PREFIX_PATH=$PREFIX    \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_FIND_ROOT_PATH=$PREFIX \
@@ -23,16 +22,16 @@ unset -f cmake
 
 emcmake cmake \
     -G "Ninja" \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DPKG_CONFIG_EXECUTABLE=$PKG_CONFIG_EXECUTABLE \
     -DSETUPTOOLS_DEB_LAYOUT=OFF \
-    -DCATKIN_SKIP_TESTING=$SKIP_TESTING \
     -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
-    -DBUILD_SHARED_LIBS=ON  \
+    -DBUILD_SHARED_LIBS=ON \
     -DBUILD_TESTING=OFF \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_DEPLOYMENT_TARGET \
     $EXTRA_CMAKE_ARGS \
     $SRC_DIR/$PKG_NAME/src/work/rmw_wasm_cpp
 
-cmake --build . --config Release --target install
+    # -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -pthread"  \
+    # -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -pthread"  \
+
+cmake --build . --config Debug --target install
